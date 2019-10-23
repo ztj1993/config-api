955ad09fe1e7561ce78bc6ed4497d80c28654809
Ztj <ztj1993@gmail.com>
2019-10-23 23:04:55 +0800

    feat: Support env var FLASK_HOST, FLASK_PORT

--- main.py
+++ main.py
@@ -3,6 +3,8 @@
 # Author: Ztj
 # Email: ztj1993@gmail.com
 
+import os
+
 from App.ApiBase import ApiBase
 from App.ApiCursor import CursorBlueprint
 from App.ApiMonitor import MonitorBlueprint
@@ -12,7 +14,10 @@ from App.FlaskErrorHandler import FlaskErrorHandler
 ApiBase = ApiBase
 FlaskErrorHandler = FlaskErrorHandler
 
+FLASK_HOST = os.getenv("FLASK_HOST", "127.0.0.1")
+FLASK_PORT = os.getenv("FLASK_PORT", 5000)
+
 if __name__ == '__main__':
     FlaskApp.register_blueprint(CursorBlueprint)
     FlaskApp.register_blueprint(MonitorBlueprint)
-    FlaskApp.run(threaded=True)
+    FlaskApp.run(host=FLASK_HOST, port=FLASK_PORT, threaded=True)
